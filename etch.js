1c19714ba167397899951f8927f55a972a8248ca
santimvera <87710111+santimvera@users.noreply.github.com>
2025-11-29 15:16:47 +0100

    add the 16x16 grid

--- etch.js
+++ etch.js
@@ -2,13 +2,16 @@ const container = document.getElementById('container');
 let gridSize = 16;
 
 function initializeSketch() {
-
-    for (let index = 0; index < gridSize; index++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        container.appendChild(square);
+    for (let rows = 0; rows < gridSize; rows++) {
+        aRow = document.createElement("div");
+        aRow.classList.add("row");
+        for (let column = 0; column < gridSize; column++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            aRow.appendChild(square);
+        }
+        container.appendChild(aRow);
     }
-
 }
 
 initializeSketch(); // This runs as soon as the script loads
