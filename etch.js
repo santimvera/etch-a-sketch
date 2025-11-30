0fbe08f8f30ac07fbac4008c2d21419eeec54160
santimvera <87710111+santimvera@users.noreply.github.com>
2025-11-30 10:34:54 +0100

    add hovering effect

--- etch.js
+++ etch.js
@@ -8,6 +8,9 @@ function initializeSketch() {
         for (let column = 0; column < gridSize; column++) {
             const square = document.createElement("div");
             square.classList.add("square");
+            square.addEventListener("mouseover", function () {
+                square.classList.add("hovered");
+            });
             aRow.appendChild(square);
         }
         container.appendChild(aRow);
